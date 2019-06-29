624ea5412d4122c09093e2583d05a5a4189258d0
baihuili <58046793@qq.com>
2019-06-29 12:54:11 +0800

    增加手写签名

--- pages/caiji/caiji.js
+++ pages/caiji/caiji.js
@@ -5,7 +5,12 @@ Page({
     toView: 999990,
     myLocation: null,
     btnDisabled: true,
-    btnType: 'primary'
+    btnType: 'primary',
+
+    context: null,
+    index: 0,
+    height: 0,
+    width: 0
   },
   onLoad: function () {
     this.setData({
@@ -270,6 +275,68 @@ Page({
       success: function (res) {
         that.onLoad();
       }
+    });
+  },
+
+  showRule: function () {
+    this.setData({
+      isRuleTrue: true
+    })
+  },
+  hideRule: function () {
+    this.setData({
+      isRuleTrue: false
+    })
+  },
+
+  /**记录开始点 */
+  bindtouchstart: function (e) {
+    this.data.context.moveTo(e.changedTouches[0].x, e.changedTouches[0].y)
+  },
+  /**记录移动点，刷新绘制 */
+  bindtouchmove: function (e) {
+
+    this.data.context.lineTo(e.changedTouches[0].x, e.changedTouches[0].y);
+    this.data.context.stroke();
+    this.data.context.draw(true);
+    this.data.context.moveTo(e.changedTouches[0].x, e.changedTouches[0].y);
+  },
+
+  /**清空画布 */
+  clear: function () {
+    this.data.context.clearRect(0, 0, this.data.width, this.data.height);
+    this.data.context.draw();
+    this.data.context.setStrokeStyle('#006600')
+    this.data.context.setLineWidth(2)
+    this.data.context.setFontSize(20)
+    let str = "签名区域";
+    this.data.context.fillText(str, Math.ceil((this.data.width - this.data.context.measureText(str).width) / 2), Math.ceil(this.data.height / 2) - 20)
+    this.data.context.draw()
+  },
+
+
+
+  onShow: function () {
+    let query = wx.createSelectorQuery();
+    const that = this;
+    query.select('#firstCanvas').boundingClientRect();
+    query.exec(function (rect) {
+      let width = rect[0].width;
+      let height = rect[0].height;
+      that.setData({
+        width,
+        height
+      });
+      const context = wx.createCanvasContext('firstCanvas')
+      that.setData({
+        context: context
+      })
+      context.setStrokeStyle('#006600')
+      context.setLineWidth(2)
+      context.setFontSize(20)
+      let str = "签名区域";
+      context.fillText(str, Math.ceil((width - context.measureText(str).width) / 2), Math.ceil(height / 2) - 20)
+      context.draw()
     });
   }
 
